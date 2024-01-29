b6ee596e86020859d207cfe87a421c3f4d48705b
Aqiilah Zayyaan Syihab <114908859+AqiilahZayyaan@users.noreply.github.com>
2024-01-29 14:06:47 +0700

    Add files via upload

---
+++ halaman/jawaban1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/css2.css">
+    <title>CatCare</title>
+</head>
+<body>
+    <div class= "border">
+        <div class ="header">
+            <p class = "judul">RING WORM</p>
+        </div>
+        <div class = "body">
+            <div class = "foto">
+                <img src ="../gambar/fotojw1.png">
+            </div>
+            
+            <p>Penyebab : </p>
+            <p>Infeksi akibat jamur yang hidup dan berkembang pada kulit kucing. Jamur yang menyebabkan penyakit ini dinamakan Microsporum sp.</p><br>
+            <p>Gejala : </p>
+            <p>- Kucing mengalami bulu rontok</p>
+            <p>- Terjadi beberapa kebotakan di area ringworm</p>
+            <p>- Sering menggaruk garuk </p><br>
+            <p>Cara Penanganan : </p>
+            <p>- Mengoleskan Obat Topikal</p>
+            <p>- Menggunakan Obat Antijamur</p>
+            <p>- Mandi Menggunakan Sampo Antijamur</p>
+            <p>- Mencukur Bulu Kucing</p>
+            <p>- Dekontaminasi Lingkungan</p>
+        </div>
+    </div>
---
+++ halaman/tanya7.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/css1.css">
+    <title>CatCare</title>
+</head>
+<body>
+    <div class ="header">
+        <p class = "judul">SELAMAT DATANG</p>
+        <p>Di SISTEM PAKAR DIAGNOSA PENYAKIT KULIT KUCING</p>
+    </div>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Proses diagnosa
+        $gejala7 = $_POST['gejala7'];
+        
+        
+        // Rule-based diagnosa
+        if ($gejala7 == 'ya') {
+            $diagnosis = header("location: jawaban4.php"); exit(); //melink ke gejala 6
+        } else {
+            $diagnosis = 'Kondisi kulit kucing tidak dapat didiagnosis dengan informasi yang diberikan.';
+        }
+
+        // Menampilkan hasil diagnosa 
+        echo "<h3>Hasil Diagnosa :</h3>";
+        echo "<p>$diagnosis</p>";
+    }
+    ?>
+    <form method="POST" action="">
+        <div class ="hf">
+            <h3>Silahkan input jawab pertanyaan dibawah ini</h3>
+        </div>
+        <div class = "pf">
+
+            <p>Apakah ada pembengkakan pada bagian dagu ?</p>
+            <div class="inputGroup">
+                <input id="radio1" name="gejala7" type="radio" value="ya" onClick='top.location="jawaban4.php"' required/>
+                <label for="radio1">ya</label>
+            </div>
+            <div class="inputGroup">
+                <input id="radio2" name="gejala7" type="radio" value="tidak" required/>
+                <label for="radio2">Tidak</label>
+            </div>
+
+            
+            <br>
+        </div>
+            <input type="submit" value="Diagnosa">
+    </form>
+</body>
+</html>
+ 
